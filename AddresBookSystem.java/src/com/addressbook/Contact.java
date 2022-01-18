e27dc8ed81a37039e346d129b06cc94f448ea83d
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2022-01-18 16:20:58 +0530

    Address book system using collections

---
+++ AddresBookSystem.java/src/com/addressbook/Contact.java
@@ -0,0 +1,25 @@
+package com.addressbook;
+
+public class Contact {
+
+	public Object getFirstName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setFirstName(String validateFirstName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setEmail(String validateEmail) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setPhoneNumber(String validatePhone) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
